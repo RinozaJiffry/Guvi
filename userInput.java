5306a8eccd5fefd236a4f55b1c7b7a156fde02d3
RinozaJiffry <66359174+RinozaJiffry@users.noreply.github.com>
2021-06-01 22:51:24 +0530

    java

---
+++ userInput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class userInput {
+    public static void main(String[] args) {
+
+        System.out.println("Sample Input : ");
+
+
+        Scanner num = new Scanner(System.in);
+        int userInput = num.nextInt();
+        System.out.println("Sample Output : \n" + userInput);
+    }
+}
